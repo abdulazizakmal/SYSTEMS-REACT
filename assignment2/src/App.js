85d96d1cc25e01a0540f7fc674a1b9a64d14ddf7
abdulazizakmal <49819149+abdulazizakmal@users.noreply.github.com>
2021-09-14 12:31:56 +0500

    Add files via upload

---
+++ assignment2/src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+// class App extends React.Component{
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 10
+//     };
+//   }
+// // using Component 
+//   componentDidMount() {
+//     document.title = `You clicked ${this.state.count} times`;
+//   }
+//   componentDidUpdate() {
+//     document.title = `You clicked ${this.state.count} times`;
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <p>You clicked {this.state.count} times</p>
+//         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+//           Click me
+//         </button>
+//       </div>
+//     );
+//   }
+   
+// }
+// export default App;
+
+
+
+
+
+
+
+
+
+// using useEffect hook 
+function App() {
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
